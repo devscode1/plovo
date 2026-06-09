9afb6c192694cc60016a668c950ae19260e34081
devscode1 <23ce137@svitvasad.ac.in>
2026-06-09 16:21:03 +0530

    commit again

--- actions/remove-workspace-member/schema.ts
+++ actions/remove-workspace-member/schema.ts
@@ -2,7 +2,6 @@ import { z } from "zod";
 
 export const RemoveWorkspaceMember = z.object({
   memberId: z.string({
-    required_error: "Member ID is required",
-    invalid_type_error: "Member ID is required",
+    error: "Member ID is required",
   }),
 });
